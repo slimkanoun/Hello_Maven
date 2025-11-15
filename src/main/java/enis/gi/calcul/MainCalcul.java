bdd53f78f0b2d71db23a84d27d963aa79d5574c1
Kanoun Slim <kanoun.slim@enis.tn>
2025-11-15 10:59:15 +0100

    add main class

---
+++ src/main/java/enis/gi/calcul/MainCalcul.java
@@ -0,0 +1,9 @@
+package enis.gi.calcul;
+
+public class MainCalcul {
+	public static void main (String [] args) {
+	  CalculMetier cm = new CalculMetier();
+	  System.out.println(cm.somme(5,10));
+	}
+
+}
