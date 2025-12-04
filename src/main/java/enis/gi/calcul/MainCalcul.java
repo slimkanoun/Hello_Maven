463d3ae09a71f239f6348f4170493aeebf8e5d75
slimkanoun <slim.kanoun@enis.tn>
2025-12-04 15:22:32 +0100

    Update MainCalcul.java

--- src/main/java/enis/gi/calcul/MainCalcul.java
+++ src/main/java/enis/gi/calcul/MainCalcul.java
@@ -4,6 +4,8 @@ public class MainCalcul {
 	public static void main (String [] args) {
 	  CalculMetier cm = new CalculMetier();
 	  System.out.println(cm.somme(5,10));
+			  System.out.println(cm.somme(5,10));
+
 	}
 
 }
